9555c1dd046841cb255f3f8bad66c2e71413a1ac
robaita <robaita2021@gmail.com>
2021-06-27 21:01:00 +0200

    removed dates

--- sections/events.php
+++ sections/events.php
@@ -18,7 +18,7 @@
                     <div class="card h-100 shadow px-2 px-lg-3 card-span pt-4">
                       <div class="text-center text-md-start card-hover">
                         <div class="card-body">
-                          <div class="d-flex align-items-center"><span class="badge bg-soft-primary text-primary fs--1 fw-light p-3 rounded-1"><span class="fw-medium fs-1 mb-2">23-25</span><br>July</span>
+                          <div class="d-flex align-items-center"><span class="badge bg-soft-primary text-primary fs--1 fw-light p-3 rounded-1"><span class="fw-medium fs-1 mb-2">TBD</span><br></span>
                             <h6 class="fw-light fs-1 fs-lg-2 text-start ms-3">Practical Machine Learning, an Online Workshop</h6>
                           </div>
                           <p class="mt-4 mb-md-0 mb-lg-3 fw-light lh-base text-start"> This is going to be the 3 day workshop. There, we would be covering the basic python and machine leanring. For more details about the course content, please click to the below link. </p>
@@ -32,7 +32,7 @@
                                 <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"></path>
                                 <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"></path>
                               </svg>
-                              <p class="mb-0 fw-light text-dark fs--1">03:00 PM - 06:00 PM IST</p>
+                              <p class="mb-0 fw-light text-dark fs--1">TBD</p>
                             </div>
                           </div>
                         </div>
