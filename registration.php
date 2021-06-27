8bb3b6098d6bb67f9a1774f024b25170dfc5be02
robaita <robaita2021@gmail.com>
2021-06-27 20:45:37 +0200

    changed title.

--- registration.php
+++ registration.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en-US" dir="ltr">
+<html class="no-js" lang="en-US" prefix="og: http://ogp.me/ns#">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
